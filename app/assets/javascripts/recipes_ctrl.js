07651e183dc54b685d3f93eb238d8b493f1f6066
Justin Jungst <jjungstprogramming@gmail.com>
2017-01-28 11:55:20 -0600

    api and json set up, can post name into database

--- app/assets/javascripts/recipes_ctrl.js
+++ app/assets/javascripts/recipes_ctrl.js
@@ -2,7 +2,26 @@ $(document).on('ready', function() {
   new Vue({
     el: '#app',
     data: {
-      message: "Hello World!"
+      message: "Hello World!",
+      recipe: {
+        name: '',
+        chef: '',
+        cooktime: '',
+        amount: '',
+        description: '',
+        favorite: false,
+      },
+      recipes: [],
+      errors: {}
+    },
+    methods: {
+      newRecipe: function(){
+        this.$http.post('/api/v1/recipes.json', this.recipe).then(function(response){
+          this.recipes.push(this.recipe);
+        }).catch(function(response){
+          this.errors = response.data.errors;
+        });
+      }
     }
   })
 })
