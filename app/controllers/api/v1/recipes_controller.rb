9173c02faa5d2090c6476c4b963037ccc08e65db
Justin Jungst <jjungstprogramming@gmail.com>
2017-02-11 15:28:21 -0600

    stopped blank ingredients from being saved in db

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -5,16 +5,22 @@ class Api::V1::RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
+
+    p "---------------------"
+    p params[:ingredients]
+    p "---------------------"
     
-    ingredients = params[:ingredients]
+    
+    ingredients = params[:ingredients] 
     ingredients.each do |ingredient|
       @recipe.ingredients.new(
         name: ingredient[:name]
       )
     end
+    
 
     if @recipe.save 
-      render :show
+      redirect_to "/recipes/#{@recipe_id}"
     else
       render json: { errors: @recipe.errors.full_messages }, status: 422
     end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   def index
 
   end
-
+  
   def show
     @recipe = Recipe.find_by(id: params[:id])
   end
